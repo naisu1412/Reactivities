734cab666cd19868d650683199027d5dfce94f9f
unknown <hikigaya1412@gmail.com>
2020-12-30 01:34:38 +0800

    using react state

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { cars } from './demo';
 import { CarItem } from './CarItem';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <ul>
-        {cars.map(car => {
-          <CarItem car={car} />
-        })}
-      </ul>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      values: [{ id: 1, name: "Value 101" }, { id: 2, name: "Value 102" }]
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>{value.name}</li>
+            ))}
+          </ul>
+        </h1>
+      </div>
+    );
+  }
+
 }
 
 export default App;
