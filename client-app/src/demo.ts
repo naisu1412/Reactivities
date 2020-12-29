62daa4c8bb0624cc56ec543bfb2203b728430fab
unknown <hikigaya1412@gmail.com>
2020-12-30 01:13:38 +0800

    added react

---
+++ client-app/src/demo.ts
@@ -0,0 +1,26 @@
+let data = 53;
+
+data = 2;
+
+export interface ICar {
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+
+const car1: ICar = {
+    color: "blue",
+    model: "bmw"
+}
+
+const car2: ICar = {
+    color: "red",
+    model: "Mercedes",
+    topSpeed: 100
+}
+
+const multiply = (x: number, y: number): string => {
+     return (x * y).toString();
+}
+
+export const cars = [car1, car2];
