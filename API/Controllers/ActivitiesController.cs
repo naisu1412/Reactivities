35ffe0dbc2f91732e26c88b9c667c81fd2607619
unknown <hikigaya1412@gmail.com>
2020-12-31 03:03:23 +0800

    added delete command

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Application.Activites;
 using Domain;
@@ -46,6 +45,12 @@ namespace API.Controllers
             return await _mediator.Send(command);
         }
 
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command { Id = id });
+        }
 
     }
 }
