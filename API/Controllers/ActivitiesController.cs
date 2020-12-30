ac225289e5fe46f5b5ef16f68b3957ad513b59e5
unknown <hikigaya1412@gmail.com>
2020-12-31 02:54:48 +0800

    added update actitivity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,9 +22,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List(CancellationToken ct)
+        public async Task<ActionResult<List<Activity>>> List()
         {
-            return await _mediator.Send(new List.Query(), ct);
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
@@ -31,6 +32,20 @@ namespace API.Controllers
         {
             return await _mediator.Send(new Details.Query { Id = id });
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
 
     }
 }
--- Application/Activites/List.cs
+++ Application/Activites/List.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activites
@@ -17,32 +16,15 @@ namespace Application.Activites
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            private readonly ILogger<List> _logger;
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context)
             {
-                _logger = logger;
                 _context = context;
-
             }
             public async Task<List<Activity>> Handle(Query request,
             CancellationToken cancellationToken)
             {
 
-                try
-                {
-                    for (var i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (Exception ex) when (ex is TaskCanceledException)
-                {
-                    _logger.LogInformation("Task Was Cancelled");
-                }
-
-                var activities = await _context.Activities.ToListAsync(cancellationToken);
+                var activities = await _context.Activities.ToListAsync();
 
                 return activities;
             }
