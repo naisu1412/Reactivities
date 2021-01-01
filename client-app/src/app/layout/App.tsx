e933a701d08402fbd1b75c8056074778461ef719
unknown <hikigaya1412@gmail.com>
2021-01-01 23:20:23 +0800

    refactor to functional rather than using class component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,45 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import 'semantic-ui-css/semantic.min.css'
 import axios from 'axios';
 import { Header, Icon, List } from 'semantic-ui-react';
 import { IActivity } from '../models/activities';
 
-interface IState {
-  activities: IActivity[]
-}
 
-class App extends Component<{}, IState> {
-  state: IState = {   //state of type IState
-    activities: []
-  }
+const App = () => {
 
-  componentDidMount() {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
     axios.get<IActivity[]>('http://localhost:5000/api/activities')
       .then((response) => {
-        this.setState({
-          activities: response.data
-        });
+        setActivities(response.data);
       });
-  }
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
+  }, []);
 
-        <List>
-          {this.state.activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-          
-        </List>
-        
-      </div>
-    );
-  }
+
+
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+
+    </div>
+  );
 
 }
 
